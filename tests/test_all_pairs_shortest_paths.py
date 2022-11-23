7fd26553d7b74f7266ca77c2a41acd620af8cbb1
karapuzio <karapuz369@gmail.com>
2022-11-24 00:47:38 +0300

    Task.

---
+++ tests/test_all_pairs_shortest_paths.py
@@ -0,0 +1,19 @@
+"""Tests for 'tasks.all_pairs_shortest_paths' module."""
+from tasks.all_pairs_shortest_paths import get_sum_of_all_pairs_shortest_paths
+
+
+def test_get_sum_of_all_pairs_shortest_paths():
+    """Tests for get_sum_of_all_pairs_shortest_paths function."""
+    assert get_sum_of_all_pairs_shortest_paths(5, [[0, 2, 0, 6, 0],
+                                                   [2, 0, 3, 8, 5],
+                                                   [0, 3, 0, 0, 7],
+                                                   [6, 8, 0, 0, 9],
+                                                   [0, 5, 7, 9, 0]]) == (80, 6)
+    assert get_sum_of_all_pairs_shortest_paths(5, [[0, 2, 0, 6, 0],
+                                                   [2, 0, 3, 8, 0],
+                                                   [0, 3, 0, 0, 0],
+                                                   [6, 8, 0, 0, 0],
+                                                   [0, 0, 0, 0, 0]]) == (38, 12)
+    assert get_sum_of_all_pairs_shortest_paths(3, [[0, 10, 5],
+                                                   [10, 0, 6],
+                                                   [5, 6, 0]]) == (42, 0)
