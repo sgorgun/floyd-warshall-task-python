e6ab21d70ecef09827646f8de6b34c2f2651f7f2
karapuzio <karapuz369@gmail.com>
2022-11-24 11:30:14 +0300

    Task.

--- tasks/all_pairs_shortest_paths.py
+++ tasks/all_pairs_shortest_paths.py
@@ -1,4 +1,4 @@
-"""Template for programming assignment: finding all-pairs shortest paths."""
+"""Template for programming assignment: Finding all-pairs shortest paths."""
 from typing import List, Tuple
 
 
@@ -6,16 +6,16 @@ def get_sum_of_all_pairs_shortest_paths(n: int, adj_matrix: List[List[int]]) -> 
     """
     Returns the tuple, where the first value is the sum of all-pairs shortest paths between all achievable vertices
     and the second is the number of the unachievable pairs of vertices in an undirected weighted graph.
-    The weight of (u,v)-edge represents a cost of traversing from 'u' to 'v'.
+    The weight of (u,v)-edge represents a distance between 'u' and 'v' and vice versa.
 
-    Edges are stored as an adjacency matrix, where 0 means no edge and positive value means edge presents and
+    Edges are stored as an adjacency matrix, where 0 means no edge and a positive value means edge presence and
     reflects its weight.
     Expected algorithm complexity is O(N^3), where N - number of vertices.
     Vertices are enumerated from 0 to N-1, there N - number of vertices.
 
     E.g. there is a graph with 4 vertices from 0 to 2 and adjacency matrix:
     [[0, 10, 5], [10, 0, 6], [5, 6, 0]]
-    Expected result is (42, 0).
+    The expected result is (42, 0).
 
     Parameters:
         n (int) : number of vertices in the graph, vertices are enumerated from 0 to n-1
